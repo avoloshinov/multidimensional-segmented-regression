39fe1c7a0ea78e53c626c7e5f1bc1fc7d2ff46dc
Anastasia Voloshinov <voloshana@gmail.com>
2019-05-01 18:31:26 -0700

    algorithm first iteration

--- src/dim_linear_merging.jl
+++ src/dim_linear_merging.jl
@@ -1,15 +1,23 @@
 using LinearAlgebra
 
-struct NodeRect
-    data::Array{Array{Float64,1},1}     # all the points that belong in this node / in this rectangle
+mutable struct NodeRect
+    data::Array{Int, 1}                 # the indices of X that belong in this rectangle
     indices::Array{Array{Float64,1},1}  # indices of the first z dimensions of the rectangle
-    theta::Array{Float64,1}
+    theta::Array{Float64,1}             # computed theta value for these points
     children::Array{Any,1}              # either an array of NodeRects, or [] if leaf
     parent::Any                         # 0 is root, a NodeRect otherwise
 end
 
 function is_leaf(node::NodeRect)
     return isempty(node.children)
+end
+
+function print_leaves(leaves::Array{NodeRect,1})
+    for leaf in leaves
+        println("leaf data is ", leaf.data)
+        println("indices are", leaf.indices)
+        println()
+    end
 end
 
 function print_tree(root::NodeRect, levels::Int, print_empty_nodes::Bool)
@@ -37,10 +45,12 @@ function print_tree(root::NodeRect, levels::Int, print_empty_nodes::Bool)
     end
 end
 
+# from array of arrays to a 2d array
 function change_to_matrix_format(X::Array{Array{Float64,1},1})
     return transpose(hcat(X...))
 end
 
+# from 2d array to array of arrays
 function change_to_array_format(X::Array{Float64,2})
     Y = Array{Array{Float64,1},1}(undef,0)
     for row=1:size(X)[1]
@@ -49,24 +59,46 @@ function change_to_array_format(X::Array{Float64,2})
     return Y
 end
 
-# array of arrays of rows -> array of arrays of columns
+# from array of arrays of rows to array of arrays of columns
 function transform_matrix(X::Array{Array{Float64,1},1})
     return [hcat(X...)[i, :] for i in 1:size(hcat(X...), 1)]
 end
 
-# fit-rectangular piece?
-function new_node(data::Array{Array{Float64,1},1}, y::Any, indices::Array{Array{Float64,1},1}, parent::Any)
-    X = change_to_matrix_format(data)
-    if y != []
-        theta = X \ y
+# from full X matrix to the subset of only the target indices
+function indices_to_sub_matrix(X::Array{Array{Float64,1},1}, target_indices::Array{Int,1})
+    Y = Array{Array{Float64,1},1}(undef,0)
+    for val in target_indices
+        push!(Y,X[val])
+    end
+    return Y
+end
+
+# from full y labels to the subset of only the target labels
+function indices_to_sub_labels(y::Array{Float64,1}, target_indices::Array{Int,1})
+    y_new = Array{Float64,1}(undef,0)
+    for val in target_indices
+        push!(y_new,y[val])
+    end
+    return y_new
+end
+
+# fits a rectangular piece to the data_indices selected from X.
+function new_node(data_indices::Array{Int,1}, X::Array{Array{Float64,1},1}, y::Any, indices::Array{Array{Float64,1},1}, parent::Any)
+    Y = indices_to_sub_matrix(X,data_indices)
+    Z = change_to_matrix_format(Y)
+
+    y_new = indices_to_sub_labels(y,data_indices)
+
+    if y_new != []
+        theta = Z \ y_new
     else
         theta = []
     end
-    return NodeRect(data,indices,theta,[],parent)
+    return NodeRect(data_indices,indices,theta,[],parent)
 end
 
 # outputs a dictionary where the keys are the possible indices
-# and the values are empty (to be filled with data points)
+# and the values are empty (to be filled with indices of corresponding data points)
 function construct_current_rectangles(z::Int, new_index_bounds::Array{Array{Array{Float64,1},1},1})
     flag=0
     count = 2^z
@@ -87,7 +119,7 @@ function construct_current_rectangles(z::Int, new_index_bounds::Array{Array{Arra
         end
     end
 
-    all_indices = Dict{Array{Array{Float64,1},1},Array{Array{Float64,1}}}()
+    all_indices = Dict{Array{Array{Float64,1},1},Array{Int64,1}}()
 
     for i=1 : 2^z
         #define the indices of one rectangle [[dim1-left,dim1-right],[dim2-left,dim2-right],etc]
@@ -110,23 +142,22 @@ function coordinate_ranking(X::Array{Array{Float64,1},1},z::Int)
     cols = transform_matrix(X)
     rankings = Array{Array{Int,1},1}(undef, 0)
     n=size(X)[1]
-    dict = Dict{Array{Float64,1},Array{Int,1}}()
     for row=1:n
         rank = Array{Int,1}(undef,0)
         for i=1:z
             index = findfirst(x->x==X[row][i],sort(cols[i]))
             push!(rank, index)
         end
-        dict[X[row]] = rank
+        push!(rankings,rank)
     end
-    return dict
+    return rankings
 end
 
-# given a parent, splits the data into 2^z children / nodeRects
-function add_children_to_parent(parent::NodeRect, z::Int, data_ordering::Dict{Array{Float64,1},Array{Int64,1}})
+# given a parent, splits the data into 2^z children/nodeRects
+function add_children_to_parent(Y::Array{Array{Float64,1},1}, parent::NodeRect, z::Int, data_ordering:: Array{Array{Int,1},1})
 
-    X = parent.data
-    n = size(X)[1]
+    X = indices_to_sub_matrix(Y,parent.data)
+    n = length(parent.data)
 
     #divide each dimension in half to prepare new dimensions for children
     new_index_bounds = Array{Array{Array{Float64,1},1},1}(undef,0)
@@ -137,13 +168,12 @@ function add_children_to_parent(parent::NodeRect, z::Int, data_ordering::Dict{Ar
         push!(new_index_bounds,[[left,middle],[middle,right]])
     end
 
-    # maps indicies of rectangle to points in that rectangle
+    # maps indicies/bounds of rectangle to the indices of points in that rectangle
     all_indices = construct_current_rectangles(z,new_index_bounds)
-    all_indices_y_vals = construct_current_rectangles(z,new_index_bounds)
 
     # for each data point, decide which child it belongs in
-    for i=1:n
-        data_ind = data_ordering[X[i]]
+    for i in parent.data
+        data_ind = data_ordering[i]
         indices = Array{Array{Float64,1},1}(undef,0)
         for j=1:z
             cur = data_ind[j]
@@ -157,13 +187,13 @@ function add_children_to_parent(parent::NodeRect, z::Int, data_ordering::Dict{Ar
                 push!(indices, [mid,upper])
             end
         end
-        push!(all_indices[indices], X[i])
-        push!(all_indices_y_vals[indices],[y[i]])
+        push!(all_indices[indices], i)
     end
 
-    for (indices, x_vals) in all_indices
-        y =vcat(all_indices_y_vals[indices]...)
-        push!(parent.children, new_node(x_vals, y, indices, parent))
+    for (indices, x_indices) in all_indices
+        if x_indices != []
+            push!(parent.children, new_node(x_indices, Y, y, indices, parent))
+        end
     end
 
     return [parent,parent.children]
@@ -171,9 +201,9 @@ end
 
 
 function create_tree(X::Array{Array{Float64,1},1}, y::Array{Float64,1}, z::Int, levels::Int)
-    n = float(size(X)[1])
-    indices = fill([0,n+1],z)
-    root = new_node(X, y, indices,0)
+    n = length(y)
+    indices = fill(float([0,n+1]),z)
+    root = new_node(collect(1:n), X, y, indices,0)
     cur_children = Array{NodeRect,1}(undef,0)
     cur_children = [root]
     cur_children_temp = Array{NodeRect,1}(undef,0)
@@ -181,19 +211,25 @@ function create_tree(X::Array{Array{Float64,1},1}, y::Array{Float64,1}, z::Int, 
     #the ordering of the first z dimensions of the data points
     data_ordering = coordinate_ranking(X,z)
 
+    all_leaves = Array{NodeRect,1}(undef,0)
+
     for level=1:levels
         for i=1:size(cur_children)[1]
-            output = add_children_to_parent(cur_children[i],z,data_ordering)
-            cur_children[i] = output[1]
-            for child in output[2]
-                push!(cur_children_temp, child)
+            if length(cur_children[i].data) > 1
+                output = add_children_to_parent(X, cur_children[i],z,data_ordering)
+                cur_children[i] = output[1]
+                for child in output[2]
+                    push!(cur_children_temp, child)
+                end
+            else
+                push!(all_leaves, cur_children[i])
             end
         end
         cur_children = cur_children_temp
         cur_children_temp = Array{NodeRect,1}(undef,0)
     end
 
-    return root
+    return root, all_leaves
 end
 
 #even number of points in each true rectange?
@@ -236,7 +272,7 @@ function generate_random_regression_data(num_rectangles::Int, n::Int, d::Int, z:
     end
 
     for i=1:n
-        data_ind = data_ordering[X[i,:]]
+        data_ind = data_ordering[i]
         indices = Array{Array{Float64,1},1}(undef,0)
         for j=1:z
             cur = data_ind[j]
@@ -283,130 +319,89 @@ end
 
 # is this correct
 function rectangle_size(p::NodeRect)
-  return size(X)[1]
+    return length(p.data)
 end
 
-function rectangle_piece_merging_error(p::NodeRect, X::Array{Float64,2}, y::Array{Float64,1}, sigma::Float64)
-  return rectangle_piece_error(p, X, y) - piece_length(p) * sigma^2
+function rectangle_piece_merging_error(p::NodeRect, X::Array{Array{Float64,1},1}, y::Array{Float64,1}, sigma::Float64)
+    new_y = indices_to_sub_labels(y, p.data)
+    new_X =indices_to_sub_matrix(X, p.data)
+    error = norm(new_y - change_to_matrix_format(new_X) * p.theta)^2
+    return error - rectangle_size(p) * sigma^2
 end
-
-# this needs to be the correct y tho... todo.....
-function recangle_piece_error(p::NodeRect, y::Array{Float64,1})
-  return norm(y - change_to_matrix_format(p.data) * p.theta)^2
-end
-
-#function linear_fit_error(X::Array{Float64,2}, y::Array{Float64,1}, left_index::Int, right_index::Int)
-#  p = fit_linear_piece(X, y, left_index, right_index)
-#  return linear_piece_error(p, X, y)
-#end
 
 function mse(yhat, ystar)
    return (1.0 / length(yhat)) * sum((yhat - ystar).^2)
 end
 
+function find_candidate_set(leaves::Array{NodeRect,1})
+    candidate_pieces = Array{NodeRect,1}(undef,0)
+    for leaf in leaves
+        parent = leaf.parent
+        if !(parent in candidate_pieces)
+            all_children_leaves = true
+            for child in parent.children
+                if !is_leaf(child)
+                    all_children_leaves = false
+                end
+            end
+            if all_children_leaves
+                push!(candidate_pieces, parent)
+            end
+        end
+    end
+    return candidate_pieces
+end
 
-##################
-#
-# function partition_to_vector(X::Array{Float64,2}, pieces::Array{LinearPiece,1})
-#   n = pieces[end].right_index
-#   (rows, cols) = size(X)
-#   if n != rows
-#     error("number of rows and rightmost index must match")
-#   end
-#   y = Array{Float64}(undef, n)
-#   for ii = 1 : length(pieces)
-#     p = pieces[ii]
-#     y[p.left_index : p.right_index] = X[p.left_index : p.right_index, :] * p.theta
-#   end
-#   return y
-# end
-#
-#
-#
-# function fit_linear_merging(X::Array{Float64,2}, y::Array{Float64,1}, sigma::Float64, num_target_pieces::Int, num_holdout_pieces::Int; initial_merging_size::Int=-1)
-#
-#   n = length(y)
-#   if initial_merging_size <= 0
-#     initial_merging_size = Int(sqrt(sqrt(n)))
-#   end
-#
-#   # Initial partition
-#   cur_pieces = Array{LinearPiece}(undef, 0)
-#   num_remaining = n
-#   cur_left = 1
-#   while num_remaining > 0
-#     cur_right = min(cur_left + initial_merging_size - 1, n)
-#     num_remaining -= initial_merging_size
-#     tmp_piece = fit_linear_piece(X, y, cur_left, cur_right)
-#     push!(cur_pieces, tmp_piece)
-#     cur_left = cur_right + 1
-#   end
-#   prev_pieces = Array{LinearPiece}(undef,0)
-#
-#   while length(cur_pieces) > num_target_pieces && length(cur_pieces) != length(prev_pieces)
-#     prev_pieces = cur_pieces
-#     cur_pieces = Array{LinearPiece}(undef, 0)
-#
-#     # Create a list of merging candidates and compute their errors
-#     candidate_pieces = Array{LinearPiece}(undef,0)
-#     candidate_errors = Array{Float64}(undef, 0)
-#     for ii = 1:floor(Int, length(prev_pieces) / 2)
-#       left_piece = prev_pieces[2 * ii - 1]
-#       right_piece = prev_pieces[2 * ii]
-#       new_piece = fit_linear_piece(X, y, left_piece.left_index, right_piece.right_index)
-#       new_error = linear_piece_merging_error(new_piece, X, y, sigma)
-#       push!(candidate_pieces, new_piece)
-#       push!(candidate_errors, new_error)
-#     end
-#
-#     # For an odd number of pieces, we directly include the last piece as a
-#     # candidate.
-#     if length(prev_pieces) % 2 == 1
-#       last_piece = prev_pieces[end]
-#       last_error = linear_piece_merging_error(last_piece, X, y, sigma)
-#       push!(candidate_pieces, last_piece)
-#       push!(candidate_errors, last_error)
-#     end
-#
-#     # Select the num_holdout_pieces'th largest error (counting from the largest
-#     # error down) as threshold.
-#     selected_threshold = max(0, length(candidate_pieces) - num_holdout_pieces + 1)
-#     error_threshold = (sort(candidate_errors))[selected_threshold]
-#
-#
-#     # Count how many of the intervals are exactly at the threshold to avoid
-#     # corner cases.
-#     num_at_threshold = 0
-#     num_above_threshold = 0
-#     for ii = 1:length(candidate_pieces)
-#       if candidate_errors[ii] == error_threshold
-#         num_at_threshold += 1
-#       elseif candidate_errors[ii] > error_threshold
-#         num_above_threshold += 1
-#       end
-#     end
-#     num_at_threshold_to_include = num_holdout_pieces - num_above_threshold
-#
-#     # Form the new partition
-#     for ii = 1:length(candidate_pieces)
-#       # Use the merge candidate if the error is small enough.
-#       if candidate_errors[ii] < error_threshold
-#         push!(cur_pieces, candidate_pieces[ii])
-#       # If the error is exactly at the threshold, we make a special check
-#       elseif candidate_errors[ii] == error_threshold && num_at_threshold_to_include > 0
-#         num_at_threshold_to_include -= 1
-#         push!(cur_pieces, candidate_pieces[ii])
-#       else
-#         # Otherwise, include the original pieces
-#         push!(cur_pieces, prev_pieces[2 * ii - 1])
-#         # Corner case for the last candidate piece, which might not be the
-#         # result of a merge
-#         if 2 * ii <= length(prev_pieces)
-#           push!(cur_pieces, prev_pieces[2 * ii])
-#         end
-#       end
-#     end
-#   end
-#
-#   return cur_pieces
-# end
+function fit_linear_merging(X::Array{Array{Float64,1},1}, y::Array{Float64,1}, sigma::Float64, z::Int, levels::Int, k::Int)
+    root, leaves = create_tree(X, y, z,levels)
+    #print_tree(root,levels,true)
+    n = length(y)
+    kp = k*(log(2,n)^2)
+
+    # identify which leaves have all siblings as leaves as well
+    # if a leaf has a parent that has all children has leaves, then this is a candidate merge
+    candidate_pieces = find_candidate_set(leaves)
+
+
+    #make sure the candidate set is large enough
+    while length(candidate_pieces) > 2*kp
+        #println("merging happening")
+        #println("candidate length", length(candidate_pieces))
+        #println("needed size", 2*kp)
+
+        # for each candidate find the error
+        candidate_errors = Array{Float64,1}(undef,0)
+        for piece in candidate_pieces
+            err = rectangle_piece_merging_error(piece, X, y, sigma)
+            push!(candidate_errors,err)
+        end
+
+        # then find the set of parents with the largest errors
+        # Select the num_holdout_pieces'th largest error (counting from the largest
+        # error down) as threshold.
+        selected_threshold = ceil(Int, length(candidate_pieces) - 2*kp + 1)
+        error_threshold = (sort(candidate_errors))[selected_threshold]
+
+        leaves_to_remove = Array{NodeRect,1}(undef,0)
+
+        # and for each of those make their kids into [] and make them into leaf nodes
+        for i=1:length(candidate_errors)
+            if candidate_errors[i] <= error_threshold
+                new_leaf_node = candidate_pieces[i]
+                for child in new_leaf_node.children
+                    push!(leaves_to_remove, child)
+                end
+                new_leaf_node.children = []
+                push!(leaves,new_leaf_node)
+            end
+        end
+
+        # remove the leaves that were merged from the leaves
+        #println("size of removing", length(leaves_to_remove))
+        #println("size of leaves currently", length(leaves))
+        leaves = filter(x -> !(x in leaves_to_remove), leaves)
+        candidate_pieces = find_candidate_set(leaves)
+    end
+
+    return leaves
+end
--- src/testing2.jl
+++ src/testing2.jl
@@ -1,38 +1,22 @@
- #using PyPlot
- #using JLD
-
-#include("dim_linear_merging.jl")
-
-#function merging_k(X::Array{Float64,2}, y::Array{Float64,1}, k::Int)
-#    return fit_linear_merging(X, y, sigma, k, floor(Int, k / 2.0), initial_merging_size=d)
-#end
-
-#sigma = 1.0
-#n_vals = round(Int, exp10.(range(2.0, 4.0, 7)) #logspace(2.0, 4.0, 7))
-#k = 5
-#d = 10
-#mses = Dict{ASCIIString, Array{Float64, 2}}()
-#times = Dict{ASCIIString, Array{Float64, 2}}()
-#n_warmup = 100
-#y, ystar, X = generate_equal_size_random_regression_data(k, n_warmup, d, sigma);
-#yhat_partition = merging_k(X, y, k)
-
 #X=[1.0 2.0 3.0 10.0 ; 9.0 3.0 5.0 7.0; 8.0 2.0 8.0 6.8; 5.0 2.2 3.3 4.0]
 #Y=[[8.0, 2.0, 8.0,6.8], [2.2,3.0,9.0,11.2],[1.0, 2.0, 3.0,7.0], [9.0, 3.0, 5.0,7.9]]
-Y=[[8.0, 2.0, 8.8,6.8], [2.2,3.4,9.0,11.2],[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99]]
-X=[[8.0, 2.0, 8.8,6.8], [2.2,3.4,9.0,11.2],[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99],[6.7,8.98,1.23,4.5],[4.55,6.44,1.11,2.22],[10.7,7.1,5.5,4.0],[4.2,3.4,6.21,9.43]]
-Z=[[8.0, 2.0, 8.8,6.8], [2.2,3.4,9.0,11.2],[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99],[6.7,8.98,1.23,4.5],[4.55,6.44,1.11,2.22]]
+#Y=[[8.0, 2.0, 8.8,6.8], [2.2,3.4,9.0,11.2],[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99]]
+#X=[[8.0, 2.0, 8.8,6.8], [2.2,3.4,9.0,11.2],[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99],[6.7,8.98,1.23,4.5],[4.55,6.44,1.11,2.22],[10.7,7.1,5.5,4.0],[4.2,3.4,6.21,9.43]]
+#Z=[[8.0, 2.0, 8.8,6.8], [2.2,3.4,9.0,11.2],[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99],[6.7,8.98,1.23,4.5],[4.55,6.44,1.11,2.22]]
 #Z=[[1.0, 2.8, 3.0,7.0], [9.6, 3.5, 5.0,7.9],[13.6, 1.55, 5.22,7.99],[6.7,8.98,1.23,4.5],[4.55,6.44,1.11,2.22],[10.7,7.1,5.5,4.0],[4.2,3.4,6.21,9.43]]
-z=2
-levels = 3
 
-num_rectangles = 4
-n = 7
-d = 4
-z = 2
+k = 4         # true number of pieces/rectangles of function
+n = 10000     # number of samples
+d = 4         # dimension of samples
+z = 2         # the number of dimensions the piecewise functions are defined in
+              # todo something breaks when z=3 right now....
 sigma = 1.0
+levels = ceil(Int,log(2,n))
 
-y, ystar, data = generate_random_regression_data(num_rectangles, n, d, z, sigma)
+y, ystar, X = generate_random_regression_data(k, n, d, z, sigma)
 
-result = create_tree(data, y, z,levels)
-print_tree(result,levels,false)
+#root, leaves = create_tree(data, y, z,levels)
+#print_tree(root,levels,true)
+
+leaves = fit_linear_merging(X, y, sigma, z, levels, k)
+length(leaves)
